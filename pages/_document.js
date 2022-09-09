4b2e3943b31c1c5b0e29a60a151698de278bfb3b
yasDesign <ingyasmani@gmail.com>
2022-09-09 04:45:35 -0400

    upload

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+
+export default function Document() {
+  return (
+    <Html>
+        <Head>
+            <link rel="stylesheet" href="css/bootstrap.min.css"/>
+        
+            <link rel="stylesheet" href="css/style.css"/>      
+            <link rel="stylesheet" href="css/responsive.css"/>
+            <link rel="icon" href="images/fevicon.png" type="image/gif" />
+            <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css"/>
+            <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css"/>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen"/>
+        </Head>
+      <body>
+        <Main />
+        <NextScript />
+
+        <Script src="js/jquery.min.js"></Script>
+        <Script src="js/bootstrap.bundle.min.js"></Script>
+        <Script src="js/jquery-3.0.0.min.js"></Script>
+        <Script src="js/jquery.mCustomScrollbar.concat.min.js"></Script>
+        <Script src="js/custom.js"></Script>
+      </body>
+    </Html>
+  )
+}
